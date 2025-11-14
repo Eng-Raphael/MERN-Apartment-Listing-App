6cc34354cbed028510c684418a304d1422486317
Raphael Alfy <eng.rafael362.alfy@gmail.com>
2025-11-14 15:08:48 +0200

    adding multiple images at once and creating image preview and test form

--- client/src/app/components/CreateApartmentForm.tsx
+++ client/src/app/components/CreateApartmentForm.tsx
@@ -22,10 +22,19 @@ export default function CreateApartmentForm() {
         if (!files) return;
 
         const arr = Array.from(files);
-        setImagesPreview(arr);
+        const updatedList = [...imagesPreview, ...arr];
+
+        setImagesPreview(updatedList);
 
         formik.setFieldValue("imagesCount", arr.length);
     };
+
+    const removeImage = (index: number) => {
+        const updated = imagesPreview.filter((_, i) => i !== index);
+        setImagesPreview(updated);
+        formik.setFieldValue("imagesCount", updated.length);
+    };
+
 
     const formik = useFormik({
         initialValues: {
@@ -53,7 +62,9 @@ export default function CreateApartmentForm() {
             referenceNo: Yup.string().required(),
             bedrooms: Yup.number().min(1).required(),
             bathrooms: Yup.number().min(1).required(),
-            deliverIn: Yup.number().required(),
+            deliverIn: Yup.number()
+                .min(new Date().getFullYear(), `Year cannot be before ${new Date().getFullYear()}`)
+                .required(),
             compound: Yup.string().required(),
             finished: Yup.string().oneOf(["FULLY", "HALF"]).required(),
             locationDescription: Yup.string().required(),
@@ -332,13 +343,25 @@ export default function CreateApartmentForm() {
                 {/* Preview */}
                 <div className="flex gap-4 mt-4 overflow-x-auto">
                     {imagesPreview.map((img, i) => (
-                        <img
-                            key={i}
-                            src={URL.createObjectURL(img)}
-                            className="w-32 h-24 object-cover rounded-lg shadow"
-                        />
-                    ))}
+                        <div key={i} className="relative">
+                    <img
+                        src={URL.createObjectURL(img)}
+                        className="w-32 h-24 object-cover rounded-lg shadow"
+                    />
+
+                    {/* REMOVE BUTTON */}
+                    <button
+                        type="button"
+                        onClick={() => removeImage(i)}
+                        className="absolute -top-2 -right-2 bg-red-600 text-white w-6 h-6
+                           rounded-full flex items-center justify-center text-xs"
+                    >
+                        ✕
+                    </button>
                 </div>
+                ))}
+        </div>
+
 
                 <button
                     type="submit"
